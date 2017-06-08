91c6ec30ccc5747dc8852d782afab9d8507d9a92
Muhammad Habib Rohman <mhrohman@live.com>
2017-06-08 23:29:08 +0700

    add(menu): add new post

--- src/components/Header/menu.js
+++ src/components/Header/menu.js
@@ -5,10 +5,11 @@ import styled from 'styled-components'
 import NewsPaperIcon from './icons/newspaper'
 import UserIcon from './icons/user'
 import GearIcon from './icons/gear'
+import PlusIcon from './icons/plus'
 import { color } from '../../constants/styles'
 
 const SCIcon = styled.div`
-  width: 35px;
+  width: 30px;
   margin: 1px auto;
 `
 
@@ -21,6 +22,7 @@ const SCUl = styled.ul`
 `
 
 const SCLi = styled.li`
+  position: relative;
   padding: 10px 0;
   flex: 1;
   align-items: center;
@@ -31,26 +33,63 @@ const SCLi = styled.li`
   svg {
     fill: ${props => props['data-active'] ? '#FFF' : color.headerLightWhite};
   }
+  > div {
+    position: relative;
+    z-index: 1;
+  }
+`
+
+const SCLiSpecial = styled(SCLi)`
+  .header-menu__icon {
+    top: 3px;
+    display: initial;
+    svg {
+      width: 55px;
+    }
+  }
+  .header-menu__label {
+    display: none;
+  }
+  &:before {
+    content: "";
+    position: absolute;
+    background: ${color.darkBlue};
+    width: 90px;
+    height: 90px;
+    left: 50%;
+    top: 0rem;
+    border-radius: 100%;
+    z-index: 0;
+    transform: translateX(-50%)
+  }
 `
 
 class HeaderMenu extends Component {
-  listMenu (label: string, icon: any, active: boolean = false) {
+  listMenu (label: string, icon: any, active: boolean = false, special: boolean = false) {
     let props = {}
     if (active) {
       props = Object.assign({}, {
         'data-active': active
       })
     }
+    let Li = SCLi
+
+    if (special) {
+      Li = SCLiSpecial
+      props = Object.assign({}, props, {
+        'data-special': true
+      })
+    }
 
     return (
-      <SCLi {...props}>
+      <Li {...props}>
         <SCIcon className="header-menu__icon">
           {icon}
         </SCIcon>
         <div className="header-menu__label">
           {label}
         </div>
-      </SCLi>
+      </Li>
     )
   }
 
@@ -59,6 +98,7 @@ class HeaderMenu extends Component {
       <SCUl className="header-menu">
         {this.listMenu('Home', <NewsPaperIcon/>, true)}
         {this.listMenu('Profile', <UserIcon/>, false)}
+        {this.listMenu('Add', <PlusIcon/>, false, true)}
         {this.listMenu('Post', <NewsPaperIcon/>, false)}
         {this.listMenu('Settings', <GearIcon/>, false)}
       </SCUl>
