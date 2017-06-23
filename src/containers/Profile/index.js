900c441e21e76a5a21fb7ac745b1fee7338a1ea3
Roman Masyhar <mhrohman@live.com>
2017-06-23 11:53:08 +0700

    add(feature): profile

--- src/containers/Profile/index.js
+++ src/containers/Profile/index.js
@@ -2,6 +2,8 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
 import styled from 'styled-components'
 
 const SCPicture = styled.div`
@@ -43,6 +45,10 @@ const SCTable = styled.table`
 `
 
 class Profile extends Component {
+  componentWillReceiveProps () {
+    this.props.data.refetch()
+  }
+
   render () {
     const { profile } = this.props
     return (
@@ -52,18 +58,18 @@ class Profile extends Component {
         />
         <div className="profile-detail">
           <SCDetail className="profile-detail__name">
-            <h2>Muhammad Habib Rohman</h2>
+            <h2>{ profile.name || 'Anonymous'}</h2>
           </SCDetail>
           <SCDetail>
             <SCTable>
               <tbody>
                 <tr>
                   <td>Posts</td>
-                  <td>12</td>
+                  <td>{ (this.props.data && this.props.data.User && this.props.data.User.posts.length) || 0 }</td>
                 </tr>
                 <tr>
-                  <td>Followers</td>
-                  <td>232</td>
+                  <td>Likes</td>
+                  <td>{ (this.props.data && this.props.data.User && this.props.data.User.likes.length) || 0 }</td>
                 </tr>
               </tbody>
             </SCTable>
@@ -81,4 +87,30 @@ function mapStateToProps (state) {
   }
 }
 
-export default connect(mapStateToProps)(Profile)
+const QueryUser = gql`
+  query getUser ($id: ID!) {
+    User (id: $id) {
+      posts {
+        id
+      }
+      likes (filter: {value: true}) {
+        id
+      }
+    }
+  }
+`
+
+const ProfileWithData = compose(
+  connect(mapStateToProps),
+  graphql(QueryUser, {
+    options: ({ profile }) => {
+      return {
+        variables: {
+          id: profile.userId || 0
+        }
+      }
+    }
+  })
+)(Profile)
+
+export default ProfileWithData
